69582c93734cea40af248e10b5df111c549ebfcb
David Flowers II <20436145+Master449@users.noreply.github.com>
2025-01-05 12:38:12 -0600

    Imported bear like git repository, on dev branch. Old repository will go into archive status

---
+++ media/js/helpers.js
@@ -0,0 +1,26 @@
+
+function showtab(tabName) {
+     var index;
+    var tabs = document.getElementsByClassName("tab");
+    console.log(tabs);
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+var size;
+Promise.all([
+    ...performance.getEntriesByType('resource').map(r =>
+        fetch(r.name, { cache: 'force-cache' })
+            .then(res => res.blob())
+            .then(blob => blob.size)
+            .catch(() => 0)
+    ),
+    Promise.resolve(new Blob([document.documentElement.outerHTML]).size)
+]).then(sizes => {
+    const size = (sizes.reduce((t, s) => t + s, 0) / 1024).toFixed(2);
+    const iphone = (5120 / size).toFixed(2);
+    document.getElementById('size').textContent = size + " Kb.";
+    document.getElementById('iphone').textContent = iphone + "x";
+});
